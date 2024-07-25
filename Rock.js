513d4fc4dc431fc63998256a23f96c6b4a04d3f0
Kal-93 <118188935+Kal-93@users.noreply.github.com>
2024-07-25 16:58:39 +0100

    Final rock commit

--- Rock.js
+++ Rock.js
@@ -1,8 +1,55 @@
-function getComputerChoice() {
-  let choices = ["Rock", "Scissors", "Paper"];
+let choices = ["Rock", "Scissor", "Paper"];
 
+function getComputerChoice() {
   let randomChoice = Math.floor(Math.random() * choices.length);
   return choices[randomChoice];
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+  let userInput = prompt(
+    "Enter your move (Rock , Paper or Scissors) (-_-) "
+  ).trim().toLowerCase;
+
+  return "You" + userInput;
+}
+
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+  function playRound(humanChoice, computerChoice) {
+    if (
+      (humanChoice === choices[0] && computerChoice === choices[1]) ||
+      (humanChoice === choices[1] && computerChoice === choices[2]) ||
+      (humanChoice === choices[2] && computerChoice === choices[0])
+    ) {
+      console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+      humanScore++;
+    } else if (humanChoice === computerChoice) {
+      console.log(`It's a tie! You both chose ${humanChoice}`);
+    } else {
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+      computerScore++;
+    }
+  }
+  for (let i = 0; i < 5; i++) {
+    let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
+    console.log(`Round ${i + 1}`);
+    console.log(`You chose: ${humanChoice}`);
+    console.log(`Computer chose: ${computerChoice}`);
+    playRound(humanChoice, computerChoice);
+  }
+  if (computerScore > humanScore) {
+    console.log(
+      `Computer beat you by ${computerScore} points to ${humanScore}!`
+    );
+  } else if (humanScore > computerScore) {
+    console.log(
+      `You beat the computer by ${humanScore} points to ${computerScore}!`
+    );
+  } else {
+    console.log(`It's a tie! Both scored ${humanScore}`);
+  }
+}
+
+playGame();
